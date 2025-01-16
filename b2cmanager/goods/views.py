e88838d32482bbfbfb15f0c06fbe2451670c1e9e
shenmu84 <shenmu84@gmail.com>
2025-01-16 19:49:19 +0800

    定义试图函数

--- b2cmanager/goods/views.py
+++ b2cmanager/goods/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from django.http import HttpRequest, HttpResponse
+#接受请求，给予相应
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
 # Create your views here.
