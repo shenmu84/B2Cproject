480fede6b3f8def935e8a8fa77b6c48f1241c909
shenmu84 <shenmu84@gmail.com>
2025-01-21 17:25:52 +0800

    ORMvsDB增删改查

--- B2C02/goods/models.py
+++ B2C02/goods/models.py
@@ -1,3 +1,42 @@
 from django.db import models
-
+'''
+id系统默认有
+默认表的名字 子应用名_类名 小写
+Object-Relational Mapping对象关系映射
+ORM         DB
+类           数据表
+对象          数据行
+属性          字段
+'''
 # Create your models here.
+class GoodInfo(models.Model):
+    #属性名对应mysql = mysql类型
+    name=models.CharField(max_length=100,unique=True)
+    pub_date=models.DateField(null=True)
+    #阅读量
+    readcount=models.IntegerField(default=0)
+#评论数
+    commentcount=models.IntegerField(default=0)
+
+    is_deleted=models.BooleanField(default=False)
+    class Meta:
+        db_table='goodsinfo'#修改表的名称
+        verbose_name="书籍管理"#admin站点使用的
+    def __str__(self):
+        return self.name
+class PeopleInfo(models.Model):
+    #定义一个有序字典
+    GENDER_CHAOICE=(
+        1,'males',
+        2,'females'
+    )
+    name=models.CharField(max_length=100,unique=True)
+    gender=models.CharField(max_length=11)
+    description=models.TextField(max_length=100,null=True)
+    is_deleted=models.BooleanField(default=False)
+
+    book=models.ForeignKey(GoodInfo,on_delete=models.CASCADE)
+    class Meta:
+        db_table='peopleinfo'
+    def __str__(self):
+        return self.name
--- B2C02/goods/views.py
+++ B2C02/goods/views.py
@@ -1,5 +1,93 @@
+from datetime import datetime
+
 from django.http import HttpResponse
 from django.shortcuts import render
+from goods.models import GoodInfo
 def index(request):
+    books=GoodInfo.objects.all()
+    print(books)
     return HttpResponse("Hello, world. You're at the polls index.")
 # Create your views here.
+'''
+增加数据
+'''
+#方式一
+from goods.models import GoodInfo
+book=GoodInfo(
+    name='Django',
+    pub_date='2000-1-1',
+    readcount=10
+)
+book.save()
+
+#方式二
+GoodInfo.objects.create(
+    name="测试卡法入门",
+    pub_date="2000-1-1",
+    readcount=100
+)
+'''
+修改数据
+'''
+#方式一
+book=GoodInfo.objects.get(id=2)
+book.name="运维开发"
+
+#方式2
+GoodInfo.objects.filter(id=3).update(name="爬虫入门",commentcount=666)
+'''
+删除数据
+'''
+#物理删除delete()和逻辑删除
+book=GoodInfo.objects.get(id=2)
+book.delete()
+GoodInfo.objects.filter(id=2).delete()
+
+'''
+查询
+'''
+#查询一个
+try:
+    book=GoodInfo.objects.get(id=2)
+except GoodInfo.DoesNotExist:
+    print("GoodInfo.DoesNotExist")
+
+#查询多个
+GoodInfo.objects.all()
+'''
+过滤查询
+id=1
+包含某个字
+以X字结尾
+书名为空
+id在几个范围之内的
+id大于3，小于3
+great gt
+litte lt
+id不等于3
+
+'''
+GoodInfo.objects.get(id=1)#id=pk=primary key
+GoodInfo.objects.filter(name_contains="爬")
+GoodInfo.objects.filter(name_endswith="程")
+GoodInfo.objects.filter(name_isnull=True)
+GoodInfo.objects.get(id_in=[1,3,5])
+GoodInfo.objects.filter(id_gt=3)
+GoodInfo.objects.exclude(id=3)
+
+'''
+F对象
+'''
+from django.db.models import F, Q
+
+#阅读量大于评论量
+GoodInfo.objects.filter(readcount_gte=F('commentcount'))
+#并且查询
+GoodInfo.objects.filter(readcount_gt=20).filter(id_lt=3)
+GoodInfo.objects.filter(readcount_gt=20,id_lt=3)#上下等价
+#或者查询
+GoodInfo.objects.filter(Q(readcount_gt=20) | Q(readcount_lt=20)|Q(readcount_gt=20))
+#并且查询
+GoodInfo.objects.filter(Q(readcount_gt=20) | Q(readcount_lt=20)& Q(readcount_gt=20))
+#非
+GoodInfo.objects.filter(~Q(readcount_gt=20))
